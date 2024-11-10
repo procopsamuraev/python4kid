26bfc0703f89e6490436ce9952cff2a712078adc
Procop <mgdinc@gmail.com>
2024-11-11 07:54:44 +0800

    update cat

--- tkinter/app_cat_calculator.py
+++ tkinter/app_cat_calculator.py
@@ -30,7 +30,7 @@ and label with printed bill
 def set_total_price(entry_name, entry_qty, entry_price, entry_day, entry_warning, entry_total_price):
     # msg_warning = ""
     name = entry_name.get().strip(' ')
-    name_valid = name.replace('.', '').replace('-', '').replace('`', '').isalnum() and len(name) >= 2
+    name_valid = name.replace(' ', '').replace('.', '').replace('-', '').replace('`', '').isalnum() and len(name) >= 2
     msg_warning = f"{name}:"
     qty = entry_qty.get().replace(' ', '')
     qty_valid = qty.isdigit() and qty > '0'
@@ -39,7 +39,7 @@ def set_total_price(entry_name, entry_qty, entry_price, entry_day, entry_warning
     day = entry_day.get().replace(' ', '')
     day_valid = day.replace('.', '', 1).isdigit() and float(day) % 0.5 == 0
     if not name_valid:
-        msg_warning = f"{msg_warning}Input more then 2 symbols in the <name> field(excluded special characters"
+        msg_warning = f"{msg_warning}Input more then 2 symbols in the name(excluded special characters"
     elif not qty_valid:
         msg_warning = f"{msg_warning}qty should be integer >0"
     elif not price_valid:
@@ -51,7 +51,8 @@ def set_total_price(entry_name, entry_qty, entry_price, entry_day, entry_warning
 
 
 def set_total_year():
-    entry_cost_annual.insert(0, float(total_price1.get())*365)
+    # entry_cost_annual.insert(0, float(entry_total_price1.get())*365)
+    entry_cost_annual.insert(0, float("entry_total_price1.get()"+"entry_price2.get()"+"entry_total_price3.get()")*365)
 
 
 def set_total_cost():
@@ -69,9 +70,8 @@ Label(text="Price,pc").grid(column=2, row=0)
 Label(text="Qty days").grid(column=3, row=0)
 Label(text="Total Price").grid(column=5, row=0)
 
-
 entry_name1 = Entry()
-entry_name1.insert(0, "eda")
+entry_name1.insert(0, "eda dry")
 entry_name1.grid(column=0, row=1)
 entry_qty1 = Entry()
 entry_qty1.insert(0, "3")
@@ -82,63 +82,67 @@ entry_price1.grid(column=2, row=1)
 entry_days1 = Entry()
 entry_days1.insert(0, "21")
 entry_days1.grid(column=3, row=1)
-# total_price1 = StringVar()
-# total_price1.set(0)
-# entry_total_price1=Entry(textvariable=total_price1)
-# entry_total_price1.grid(column=5, row=1)
-# total_price1 = StringVar()
-# total_price1.set(0)
-entry_total_price1=Entry()
-# entry_total_price1.insert(0,"0.00")
+entry_total_price1 = Entry()
 entry_total_price1.grid(column=5, row=1)
-entry_warning1= Entry()
+entry_warning1 = Entry()
 entry_warning1.insert(0, '')
 entry_warning1.grid(column=0, columnspan=4, sticky='nsew', row=2)
 button_1 = Button(text="=", command=lambda : set_total_price(entry_name1, entry_qty1, entry_price1, entry_days1, entry_warning1, entry_total_price1))
 button_1.grid(column=4, row=1)
 
-# entry_21 = Entry()
-# entry_21.grid(column=0, row=2)
-# entry_22 = Entry()
-# entry_22.grid(column=1, row=2)
-# entry_23 = Entry()
-# entry_23.grid(column=2, row=2)
-# entry_24 = Entry()
-# entry_24.grid(column=3, row=2)
-# entry_25 = Entry()
-# entry_25.grid(column=5, row=2)
-# button_2 = Button(text="=")
-# button_2.grid(column=4, row=2)
-#
-# entry_31 = Entry()
-# entry_31.grid(column=0, row=3)
-# entry_32 = Entry()
-# entry_32.grid(column=1, row=3)
-# entry_33 = Entry()
-# entry_33.grid(column=2, row=3)
-# entry_34 = Entry()
-# entry_34.grid(column=3, row=3)
-# entry_35 = Entry()
-# entry_35.grid(column=5, row=3)
-# button_2 = Button(text="=")
-# button_2.grid(column=4, row=3)
+entry_name2 = Entry()
+entry_name2.insert(0, "eda conservy")
+entry_name2.grid(column=0, row=3)
+entry_qty2 = Entry()
+entry_qty2.insert(0, "1")
+entry_qty2.grid(column=1, row=3)
+entry_price2 = Entry()
+entry_price2.insert(0, "100.80")
+entry_price2.grid(column=2, row=3)
+entry_days2 = Entry()
+entry_days2.insert(0, "1")
+entry_days2.grid(column=3, row=3)
+entry_total_price2 = Entry()
+entry_total_price2.grid(column=5, row=3)
+entry_warning2 = Entry()
+entry_warning2.grid(column=0, columnspan=4, sticky='nsew', row=4)
+button_2 = Button(text="=", command=lambda: set_total_price(entry_name2, entry_qty2, entry_price2, entry_days2, entry_warning2, entry_total_price2))
+button_2.grid(column=4, row=3)
 
-Label(text="Всего затраты за год:", justify=RIGHT).grid(column=0, row=4, columnspan=4)
+entry_name3 = Entry()
+entry_name3.insert(0, "napolnitel")
+entry_name3.grid(column=0, row=5)
+entry_qty3 = Entry()
+entry_qty3.insert(0, "1")
+entry_qty3.grid(column=1, row=5)
+entry_price3 = Entry()
+entry_price3.insert(0, "300.80")
+entry_price3.grid(column=2, row=5)
+entry_days3 = Entry()
+entry_days3.insert(0, "7")
+entry_days3.grid(column=3, row=5)
+entry_total_price3 = Entry()
+entry_total_price3.grid(column=5, row=5)
+entry_warning3 = Entry()
+entry_warning3.grid(column=0, columnspan=4, sticky='nsew', row=6)
+button_3 = Button(text="=", command=lambda : set_total_price(entry_name3, entry_qty3, entry_price3, entry_days3, entry_warning3, entry_total_price3))
+button_3.grid(column=4, row=5)
+
+
+Label(text="Всего затраты за год:", justify=RIGHT).grid(column=0, row=7, columnspan=4)
 button_6 = Button(text="=",command=set_total_year)
-button_6.grid(column=4, row=4)
+button_6.grid(column=4, row=7)
 entry_cost_annual = Entry()
-entry_cost_annual.grid(column=5, row=4)
+entry_cost_annual.grid(column=5, row=7)
 
-Label(text="Period, years:", justify=RIGHT).grid(column=0, row=5, columnspan=4)
-# button_6 = Button(text="=")
-# button_6.grid(column=4, row=4)
+Label(text="Period, years:", justify=RIGHT).grid(column=0, row=8, columnspan=4)
 entry_years = Entry()
-entry_years.grid(column=5, row=5)
+entry_years.grid(column=5, row=8)
 entry_years.insert(0, "15")
 
-Label(text="Total cost for whole period:", justify=RIGHT).grid(column=0, row=6, columnspan=4)
+Label(text="Total cost for whole period:", justify=RIGHT).grid(column=0, row=9, columnspan=4)
 button_6 = Button(text="=", command=set_total_cost)
-button_6.grid(column=4, row=6)
+button_6.grid(column=4, row=9)
 entry_for_full= Entry()
-entry_for_full.grid(column=5, row=6)
+entry_for_full.grid(column=5, row=9)
 root.mainloop()
