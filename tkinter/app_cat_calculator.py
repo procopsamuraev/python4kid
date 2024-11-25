3ec6dc89336d36507c08d6ec2f6b49db86ef16b8
Procop <mgdinc@gmail.com>
2024-11-25 22:33:18 +0800

    app tictac0.9

--- tkinter/app_cat_calculator.py
+++ tkinter/app_cat_calculator.py
@@ -34,16 +34,16 @@ def get_report_error(name, qty, price, day):
     day_valid = day.rstrip('05').removesuffix('.').isdigit() and day > '0'
     msg_warning = ""
     if not name and not qty and not price and not day:
-        msg_warning = f"All field are empty - ignoring the line"
+        msg_warning = "Empty_line"
         return msg_warning
     if not name_valid:
-        msg_warning = f"{msg_warning}Fill up 'Product' field with 2 or more alphabet symbols"
+        msg_warning = f"{msg_warning}Fill up 'Product' field with 2 or more alphabet symbols\n"
     if qty_false:
-        msg_warning = f"{msg_warning}Fill up 'Qty' int, positive"
+        msg_warning = f"{msg_warning}\nFill up 'Qty' int, positive\n"
     if not price_valid:
-        msg_warning = f"{msg_warning}Fill up 'Price' with positive number and not more then 2 digits after dot"
+        msg_warning = f"{msg_warning}\nFill up 'Price' with positive number and not more then 2 digits after dot\n"
     if not day_valid:
-        msg_warning = f"{msg_warning}Fill up  'Day' with positive number and delimoe na 0,5"
+        msg_warning = f"{msg_warning}\nFill up 'Day' with positive number and be dividing by 0,5\n"
     return msg_warning
 
 
@@ -56,10 +56,15 @@ def set_total_price(list_entries):
     entry_total_price.delete(0, 'end')
     msg_warning = get_report_error(name, qty, price, day)
     label_warning.config(text='')
-    if msg_warning:
-        label_warning.config(text = msg_warning)
+    if msg_warning == 'Empty_line':
+        label_warning.grid_forget()
         entry_total_price.insert(0, '0')
-    else:
+    if msg_warning and msg_warning != 'Empty_line':
+        label_warning.grid(column=0, columnspan=6, sticky='nsew', row=2)
+        label_warning.config(text=msg_warning)
+        entry_total_price.insert(0, 'NA')
+    elif not msg_warning:
+        label_warning.grid_forget()
         entry_total_price.insert(0, str(round(int(qty)*float(price)/float(day), 2)))
         return name, qty, price, day, entry_total_price.get()
 
@@ -69,44 +74,52 @@ def set_total_annual():
     set_total_price(list_entries1)
     set_total_price(list_entries2)
     set_total_price(list_entries3)
-    print(float(entry_total_price1.get()))
-    print(float(entry_total_price2.get()))
-    print(float(entry_total_price3.get()))
-    daily_total= float(entry_total_price1.get())+float(entry_total_price2.get())+float(entry_total_price3.get())
-    entry_cost_annual.insert(0, daily_total*365)
+    price1, price2, price3 = entry_total_price1.get(), entry_total_price2.get(), entry_total_price3.get()
+    if (price1 or price2 or price3) == "NA":
+        label_warning_global.config(text="Total Annual: Some errors with on of the product")
+        label_warning_global.grid(column=0, columnspan=6, sticky='nsew', row=20)
+    else:
+        label_warning_global.grid_forget()
+        entry_cost_annual.insert(0, str(round((float(price1)+float(price2)+float(price3)) * 365, 2)))
 
 
 def set_total_cost():
-    msg_warning=''
+    msg_warning = ''
     set_total_annual()
     cost_annual = entry_cost_annual.get()
     years = entry_years.get().replace(' ', '')
-    years_valid = years.isdigit() and years > '0'
-    if not years_valid:
-        msg_warning = f"{msg_warning} vvedite years < 0 < 99" # ydo not show this message at all
+    data_true = years.isdigit() and years > '0' and cost_annual.replace('.', '').isdigit()
+    entry_for_full.delete(0, 'end')
+    if data_true:
+        label_warning_global.grid_forget()
+        entry_for_full.insert(0, str(round(float(cost_annual)*int(years), 2)))
     else:
-        entry_for_full.delete(0, 'end')
-        entry_for_full.insert(0, str(round(float(cost_annual) * int(years))))
+        label_warning_global.config(text="Total cost: Some errors with on of the product")
+        label_warning_global.grid(column=0, columnspan=6, sticky='nsew', row=20)
 
 
 def print_bill():
     set_total_cost()
-    output = Label(font=('Ubuntu Mono', 10))
-    width = int(entry_bill_width.get().strip(' ')) if entry_bill_width.get().strip(' ').isdigit() else '180'
-    col_width = round(width/6)
-    bill_string = f"{root.title().center(width)}\n"
-    bill_string = f"{bill_string}{label0['text'].ljust(col_width*2)}{label1['text'].ljust(col_width)}{label2['text'].ljust(col_width)}{label3['text'].ljust(col_width)}{label5['text'].ljust(col_width)}\n"
-    name, qty, price, day, price_total = set_total_price(list_entries1)
-    bill_string = f"{bill_string}{name.ljust(col_width*2)}{qty.ljust(col_width)}{price.ljust(col_width)}{day.ljust(col_width)}{price_total.ljust(col_width)}\n"
-    name, qty, price, day, price_total = set_total_price(list_entries2)
-    bill_string = f"{bill_string}{name.ljust(col_width*2)}{qty.ljust(col_width)}{price.ljust(col_width)}{day.ljust(col_width)}{price_total.ljust(col_width)}\n"
-    name, qty, price, day, price_total = set_total_price(list_entries3)
-    bill_string = f"{bill_string}{name.ljust(col_width*2)}{qty.ljust(col_width)}{price.ljust(col_width)}{day.ljust(col_width)}{price_total.ljust(col_width)}\n"
-    bill_string = f"{bill_string}{label_annual['text'].rjust(col_width*5-1)} {entry_cost_annual.get().ljust(col_width)}\n"
-    bill_string = f"{bill_string}{label_years['text'].rjust(col_width*5-1)} {entry_years.get().ljust(col_width)}\n"
-    bill_string = f"{bill_string}{label_cost_total['text'].rjust(col_width*5-1)} {entry_for_full.get().ljust(col_width)}\n"
-    output.config(text=bill_string)
-    output.grid(row=12, columnspan=6, sticky='nsew')
+    if not entry_cost_annual.get().replace('.', '').isdigit():
+        label_bill.config(text="Bill: Some errors with on of the product")
+        label_bill.grid(column=0, columnspan=6, sticky='nsew', row=20)
+        label_bill.grid_forget()
+    else:
+        width = int(entry_bill_width.get().strip(' ')) if entry_bill_width.get().strip(' ').isdigit() else '180'
+        col_width = round(width/6)
+        bill_string = f"{root.title().center(width)}\n"
+        bill_string = f"{bill_string}{label0['text'].ljust(col_width*2)}{label1['text'].ljust(col_width)}{label2['text'].ljust(col_width)}{label3['text'].ljust(col_width)}{label5['text'].ljust(col_width)}\n"
+        name, qty, price, day, price_total = set_total_price(list_entries1)
+        bill_string = f"{bill_string}{name.ljust(col_width*2)}{qty.ljust(col_width)}{price.ljust(col_width)}{day.ljust(col_width)}{price_total.ljust(col_width)}\n"
+        name, qty, price, day, price_total = set_total_price(list_entries2)
+        bill_string = f"{bill_string}{name.ljust(col_width*2)}{qty.ljust(col_width)}{price.ljust(col_width)}{day.ljust(col_width)}{price_total.ljust(col_width)}\n"
+        name, qty, price, day, price_total = set_total_price(list_entries3)
+        bill_string = f"{bill_string}{name.ljust(col_width*2)}{qty.ljust(col_width)}{price.ljust(col_width)}{day.ljust(col_width)}{price_total.ljust(col_width)}\n"
+        bill_string = f"{bill_string}{label_annual['text'].rjust(col_width*5-1)}={entry_cost_annual.get().ljust(col_width)}\n"
+        bill_string = f"{bill_string}{label_years['text'].rjust(col_width*5-1)}={entry_years.get().ljust(col_width)}\n"
+        bill_string = f"{bill_string}{label_cost_total['text'].rjust(col_width*5-1)}={entry_for_full.get().ljust(col_width)}\n"
+        label_bill.config(text=bill_string)
+        label_bill.grid(row=12, columnspan=6, sticky='nsew')
 
 
 root = Tk()
@@ -138,7 +151,6 @@ entry_total_price1 = Entry()
 entry_total_price1.insert(0, '0.00')
 entry_total_price1.grid(column=5, row=1)
 label_warning1 = Label()
-label_warning1.grid(column=0, columnspan=4, sticky='nsew', row=2)
 list_entries1 = [entry_name1, entry_qty1, entry_price1, entry_days1, label_warning1, entry_total_price1]
 button_1 = Button(text="=", command=lambda: set_total_price(list_entries1))
 button_1.grid(column=4, row=1)
@@ -159,7 +171,6 @@ entry_total_price2 = Entry()
 entry_total_price2.insert(0, '0.00')
 entry_total_price2.grid(column=5, row=3)
 label_warning2 = Label()
-label_warning2.grid(column=0, columnspan=4, sticky='nsew', row=4)
 list_entries2 = [entry_name2, entry_qty2, entry_price2, entry_days2, label_warning2, entry_total_price2]
 button_2 = Button(text="=", command=lambda: set_total_price(list_entries2))
 button_2.grid(column=4, row=3)
@@ -180,7 +191,6 @@ entry_total_price3 = Entry()
 entry_total_price3.insert(0, '0.00')
 entry_total_price3.grid(column=5, row=5)
 label_warning3 = Label()
-label_warning3.grid(column=0, columnspan=4, sticky='nsew', row=6)
 list_entries3 = [entry_name3, entry_qty3, entry_price3, entry_days3, label_warning3, entry_total_price3]
 button_3 = Button(text="=", command=lambda: set_total_price(list_entries3))
 button_3.grid(column=4, row=5)
@@ -206,10 +216,16 @@ button_6.grid(column=4, row=9)
 entry_for_full= Entry()
 entry_for_full.grid(column=5, row=9)
 
+label_warning_global = Label()
+
 Label(text="Print bill with width:", justify=RIGHT).grid(column=0, row=10, columnspan=4)
 button_print_bill = Button(text="Print", command=print_bill)
 button_print_bill.grid(column=4, row=10)
 entry_bill_width = Entry()
 entry_bill_width.grid(column=5, row=10)
 entry_bill_width.insert(0, '120')
+
+label_bill = Label(font=('Ubuntu Mono', 10))
+
 root.mainloop()
+
--- tkinter/app_tictac.py
+++ tkinter/app_tictac.py
@@ -1,8 +1,32 @@
 import tkinter
 from tkinter import *
 from tkinter.ttk import *
-
 turn = 1
+
+"""
+
+2x2 combinations
+00 01
+10 11
+00 10
+01 11
+00 11
+01 10
+
+
+3x3 combinations
+00 01 02
+10 11 12
+20 21 22
+
+00 10 20
+01 11 11
+02 12 22
+
+00 11 22
+02 11 20
+
+"""
 
 
 def set_button_press(button):
@@ -12,34 +36,42 @@ def set_button_press(button):
     if not str(button.cget('text')).isalnum():
         button.config(text='X') if turn % 2 != 0 else button.config(text='O')
         turn = turn + 1
-
-        buttons_combinations.remove(button)
-        for buttons in buttons_combinations:
-            check_winner(button, buttons)
-
+    check_winner()
 
 def clear_fields():
     for button in button_00, button_10, button_11, button_01:
         button.config(text='')
 
 
-def check_winner(button1, button2):
-    if button1.cget('text') == button2.cget('text'):
-        print(f"{button1.cget('text')}-Won!")
-
+def check_winner():
+    if button_00.cget('text') == button_01.cget('text') == button_02.cget('text') and button_00.cget('text').isalnum()) or
+    elif button_10.cget('text') == button_11.cget('text') == button_12.cget('text') and button_10.cget('text').isalnum():
+        print(f"{button_00.cget('text')}-WON!")
+    else:
+        print("1")
 
 
 root = Tk()
-root.title('TicTac v0.9')
+root.title('TicTacToe v0.9')
 button_00 = Button(text="", command=lambda: set_button_press(button_00))
 button_00.grid(column=0, row=0, ipadx=80, ipady=100)
 button_01 = Button(text="", command=lambda: set_button_press(button_01))
 button_01.grid(column=0, row=1, ipadx=80, ipady=100)
+button_02 = Button(text="", command=lambda: set_button_press(button_02))
+button_02.grid(column=0, row=2, ipadx=80, ipady=100)
 button_10 = Button(text="", command=lambda: set_button_press(button_10))
 button_10.grid(column=1, row=0, ipadx=80, ipady=100)
 button_11 = Button(text="", command=lambda: set_button_press(button_11))
 button_11.grid(column=1, row=1, ipadx=80, ipady=100)
-button_new_game = Button(text="New Game", command= clear_fields)
-button_new_game.grid(columnspan=2, row=2,ipadx=200, ipady=10)
+button_12 = Button(text="", command=lambda: set_button_press(button_12))
+button_12.grid(column=1, row=2, ipadx=80, ipady=100)
+button_20 = Button(text="", command=lambda: set_button_press(button_20))
+button_20.grid(column=2, row=0, ipadx=80, ipady=100)
+button_21 = Button(text="", command=lambda: set_button_press(button_21))
+button_21.grid(column=2, row=1, ipadx=80, ipady=100)
+button_22 = Button(text="", command=lambda: set_button_press(button_22))
+button_22.grid(column=2, row=2, ipadx=80, ipady=100)
+button_new_game = Button(text="New Game", command=clear_fields)
+button_new_game.grid(columnspan=3, row=3,ipadx=200, ipady=10)
 
 root.mainloop()
