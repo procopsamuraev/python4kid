f5a88f022a14c838e062bfb61463c7d304d4c62e
Procop <mgdinc@gmail.com>
2025-01-02 18:05:25 +0800

    tkinter text ex4

--- tkinter/tkinter_text.py
+++ tkinter/tkinter_text.py
@@ -10,6 +10,11 @@ def copy_all():
     text_buffer = text.get(0.0, END)
 
 
+def copy_selected():
+    global text_buffer
+    text_buffer = text.selection_get()
+
+
 def paste_buffer():
     global text_buffer
     text.insert(INSERT, text_buffer)
@@ -25,6 +30,8 @@ frame_button.pack()
 # button = Button(text='Add', command=add_text)
 button_copy_all = Button(frame_button, text='Copy all', command=copy_all)
 button_copy_all.pack(side=LEFT)
+button_copy_selected = Button(frame_button, text='Copy', command=copy_selected)
+button_copy_selected.pack(side=LEFT)
 button_paste_buffer = Button(frame_button, text='Paste', command=paste_buffer)
 button_paste_buffer.pack(side=LEFT)
 
