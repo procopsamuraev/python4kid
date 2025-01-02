df2aee961338cba7c1b000536b5161a2ec3c0e6f
Procop <mgdinc@gmail.com>
2025-01-02 22:51:38 +0800

    tkinter text ex7

--- tkinter/tkinter_text.py
+++ tkinter/tkinter_text.py
@@ -1,6 +1,8 @@
 from tkinter import *
 
 text_buffer = ''
+
+
 def add_text():
     text.insert(END, 'This is a new text!')
 
@@ -10,24 +12,49 @@ def copy_all():
     text_buffer = text.get(0.0, END)
 
 
-def copy_selected():
-    global text_buffer
-    text_buffer = text.selection_get()
- 
+def copy():
+    if text.tag_ranges('sel'):
+        return text.selection_get()
+    else: 
+        None
+    
 
 def delete():
-    text.delete(SEL_FIRST, SEL_LAST)
+    text.delete(SEL_FIRST, SEL_LAST) if text.tag_ranges('sel') else None
 
 
 def cut():
-    copy_selected()
-    delete()
+    global text_buffer
+    text_buffer = copy()
+    if text_buffer:
+        delete()
     
 
-def paste_buffer():
+def paste():
     global text_buffer
-    text.insert(INSERT, text_buffer)
-    
+    text.insert(INSERT, text_buffer) if text_buffer else None
+
+
+def caps():
+    cut()
+    text.insert(INSERT, text_buffer.capitalize()) if text_buffer else None
+
+
+def lower():
+    cut()
+    text.insert(INSERT, text_buffer.lower()) if text_buffer else None
+
+
+def upper():
+    cut()
+    text.insert(INSERT, text_buffer.upper()) if text_buffer else None
+
+
+def title():
+    cut()
+    text.insert(INSERT, text_buffer.title()) if text_buffer else None
+
+
 
 root = Tk()
 text = Text(root) 
@@ -39,12 +66,20 @@ frame_button.pack()
 # button = Button(text='Add', command=add_text)
 button_copy_all = Button(frame_button, text='Copy all', command=copy_all)
 button_copy_all.pack(side=LEFT)
-button_copy_selected = Button(frame_button, text='Copy', command=copy_selected)
-button_copy_selected.pack(side=LEFT)
+button_copy = Button(frame_button, text='Copy', command=copy)
+button_copy.pack(side=LEFT)
 button_cut= Button(frame_button, text='Cut', command=cut)
 button_cut.pack(side=LEFT)
-button_paste_buffer = Button(frame_button, text='Paste', command=paste_buffer)
+button_paste_buffer = Button(frame_button, text='Paste', command=paste)
 button_paste_buffer.pack(side=LEFT)
+button = Button(frame_button, text='Capitalize', command=caps)
+button.pack(side=LEFT)
+button = Button(frame_button, text='Lower', command=lower)
+button.pack(side=LEFT)
+button = Button(frame_button, text='Upper', command=upper)
+button.pack(side=LEFT)
+button = Button(frame_button, text='Title', command=title)
+button.pack(side=LEFT)
 
 
 root.mainloop()
