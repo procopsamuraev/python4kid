d8dddcff3a50c2cb59eb0ae2582d08af96a7d015
Procop <mgdinc@gmail.com>
2025-01-03 10:45:16 +0800

    tkinter text ex8

--- tkinter/tkinter_text_status.py
+++ tkinter/tkinter_text_status.py
@@ -10,12 +10,24 @@ def get_status():
     label.config(text=line_status)
 
 
+def backspace():
+    a, b = text.index('insert').split('.')
+    if b=='0' and a>'0': 
+        index0=f"{int(a)-1}.1000" #1000 should be replaced to the last symbol on string-1
+    else: 
+        index0=f"{a}.{int(b)-1}"
+    print(index0)
+    text.delete(index0, f"{a}.{b}")
+
+
 root = Tk()
 text = Text(root) 
 text.insert(INSERT, "Some Text....some more text....end more..\n.........\n.............12345678911232sssssssssssssssssssssss")
 text.pack(expand=1, fill='both')
 frame_button = LabelFrame(text='', border=0, width=100)
 frame_button.pack(expand=1, fill='x')
+button = Button(frame_button, text='<- BackSpace', command=backspace)
+button.pack(side=LEFT)
 button = Button(frame_button, text='Get Status', command=get_status)
 button.pack(side=LEFT)
 label = Label(frame_button, justify=CENTER, text='')
