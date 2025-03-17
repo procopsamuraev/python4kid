a8d6a243fcc71c611da28de6da1f81e72b5b853b
Procop <mgdinc@gmail.com>
2025-03-17 21:38:03 +0800

    chess while moves

--- tkinter/app_chessboard_while.py
+++ tkinter/app_chessboard_while.py
@@ -1,8 +1,16 @@
 import tkinter
 from tkinter import *
+import tkinter.font as font
 
 list_fields = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', '']
 list_figures = ['P','R','N','B', 'Q', 'K', 'B', 'N', 'R', 'P']
+list_move = []
+font_default = ('Helvetica 12 bold')
+font_selected = ('Helvetica 12 bold italic')
+
+
+#create Font object
+
 # c
 # o 
 # l
@@ -18,13 +26,22 @@ list_figures = ['P','R','N','B', 'Q', 'K', 'B', 'N', 'R', 'P']
 # 
 
 
-def move_figure():
+def move_figure(column, row):
+    square_selected = list_square[row-1][column-1]
+    square_selected.config(font=font_selected)
+    list_move.append(square_selected)
+    if len(list_move) == 2:
+        color = list_move[0]['foreground']
+        figure = list_move[0]['text']
+        list_move[0].config(text='')
+        list_move[-1].config(text=figure, foreground=color, font=font_default)
+        list_move.clear()
+
 
 
 
 root = Tk()
 root.title("Chessboard")
-image_square = tkinter.PhotoImage(width=1, height=1)
 width = 2
 length=len(list_fields)
 max_size_field = len(list_fields)-1
@@ -32,7 +49,6 @@ list_square = []
 list_square_line = []
 
 # draw board
-
 i = 0 
 j = 0
 while i < length*length:
@@ -44,29 +60,24 @@ while i < length*length:
     elif column == 0 or column == max_size_field:
         Label(text=f"{max_size_field-row}", bg='white', width=width).grid(column=column, row=row)
     else:
-        regular_square = Button(text='', bg=color, image=image_square, width=50, height=50, font='bold', command=)
+        regular_square = Button(text='', bg=color, width=6, height=4, font=font_default, command=lambda row=row, column=column: move_figure(column, row))
         regular_square.grid(column=column, row=row)
         list_square_line.append(regular_square)
         # creating list of list 
-        if j == 7: 
+        j += 1
+        if j == 8: 
             list_square.append(list_square_line)
             list_square_line = []
             j = 0;
-        j += 1
+        # set board with figures
         if row == 1: 
             regular_square.config(text=list_figures[i%length], foreground='black', compound='c')
         if row == 2: 
             regular_square.config(text=list_figures[0], foreground='black', compound='c')
         elif row == 7: 
-            regular_square.config(text=list_figures[0], foreground='gray', font='bold', compound='c')
+            regular_square.config(text=list_figures[0], foreground='gray', compound='c')
         elif row == 8: 
             regular_square.config(text=list_figures[i%length], foreground='gray', compound='c')
-            
-    
     i += 1
-
-
-
-# list_square[1][1].config(text='t')
 
 root.mainloop()
