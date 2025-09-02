35e4ebd00e59adb84f6f4d159203ce00cfefa48e
mgdinc@gmail.com <mgdinc@gmail.com>
2025-09-03 07:24:56 +0800

    app-rainbow-class

--- tkinter/app_rainbow_class.py
+++ tkinter/app_rainbow_class.py
@@ -1,18 +1,25 @@
 from tkinter import *
 
+class ButtonRainbow:
+    font_text: str = ('Ubuntu Mono', 12)
 
-def display_color(_desc, color):
-    label.config(text=_desc)
-    entry_color_code.set(color)
-    entry_color.config(background=color)
+    def __init__(self, parent: Tk, color: str = 'white', color_code: str = '#ff0000', text: str = '', color_code_var: StringVar = None, label: Label = None):
+        self.color = color
+        self.color_code = color_code
+        self.text = text
+        self.color_code_var = color_code_var
+        self.label = label
+        Button(parent, font=ButtonRainbow.font_text, text=self.text, bg=self.color_code, command=self.draw_button).pack(fill='x')
 
-
+    def draw_button(self):
+        self.color_code_var.set(self.color_code)
+        self.label.config(text=self.color, background=self.color)
 
 list_colors = [
     ['red', '#ff0000'],
     ['orange', '#ff7d00'],
     ['yellow', '#ffff00'],
-    ['green', '#00ff00'], 
+    ['green', '#00ff00'],
     ['lightblue', '#007dff'],
     ['blue', '#0000ff'],
     ['purple', '#7d00ff']
@@ -20,14 +27,13 @@ list_colors = [
 
 root = Tk()
 root.title("Colors")
-label = Label(text="Color name", font=15, fg="black" )
-label.pack()
-entry_color_code = StringVar()
-entry_color = Entry(textvariable=entry_color_code, justify="center", font=20, fg="black")
-entry_color.pack()
-entry_color_code.set("Color code")
+label_color = Label(text="Color name", font=ButtonRainbow.font_text, fg="black" )
+label_color.pack(fill='x')
+string_code_color = StringVar()
+Entry(textvariable=string_code_color, justify="center", fg="black").pack(fill='x')
+string_code_color.set("Color code")
 
-for index, color_combo in enumerate(list_colors): 
-    Button(root, text=index, font=15, bg=color_combo[1], command = lambda _params = color_combo :display_color(*_params)).pack(fill=X)
+for index, color_combo in enumerate(list_colors):
+    ButtonRainbow(root, text=str(index+1), color= color_combo[0], color_code=color_combo[1], color_code_var=string_code_color, label=label_color)
 
 root.mainloop()
